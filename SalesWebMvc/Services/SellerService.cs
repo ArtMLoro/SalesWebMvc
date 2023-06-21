c2c82050d98328487b8d84e94e3c7d963f459a70
Arthur Minella <arthur.minella@gmail.com>
2023-06-21 20:31:32 -0300

    Foreign key not null (referencial integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -21,9 +21,10 @@ namespace SalesWebMvc.Services
             return _context.Seller.ToList();
         }
 
-        public void Insert(Seller Obj)
+        public void Insert(Seller obj)
         {
-            _context.Add(Obj);
+            obj.Department = _context.Department.First();
+            _context.Add(obj);
             _context.SaveChanges();
         }
     }
